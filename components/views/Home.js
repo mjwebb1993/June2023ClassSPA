8a6117c930ebbf6b85adfd415a05433ba4818ed9
Jon Skeen <jonskeen@gmail.com>
2023-07-19 10:50:08 -0700

    add 6.2 material

---
+++ components/views/Home.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+
+export const Home = () => html`
+  <section id="jumbotron">
+    <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
+    <a href="index.html">"Call to Action" "Button"</a>
+  </section>
+`;
+
+export default Home;
---
+++ components/views/Pizza.js
@@ -0,0 +1,17 @@
+import html from "html-literal";
+
+export const Pizza = () => html`
+  <section id="pizza">
+    <table id="pizzas">
+      <tr>
+        <th>Crust</th>
+        <th>Cheese</th>
+        <th>Sauce</th>
+        <th>Toppings</th>
+        <th>Customer</th>
+      </tr>
+    </table>
+  </section>
+`;
+
+export default Pizza;
