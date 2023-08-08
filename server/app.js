08a7d3a3bbcdede4d2d69e001ceb8ac21011750a
Matt Thomas <matsinet@gmail.com>
2023-08-08 08:00:51 -0500

    Master -> Development

--- server/app.js
+++ server/app.js
@@ -1,21 +1,99 @@
-// 'Import' the http module
-import http from "http";
+// 'Import' the Express module instead of http
+import express from "express";
+import dotenv from "dotenv";
+// Initialize the Express application
+const app = express();
 
-// Initialize the http server
-const server = http
-  .createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// Load environment variables from .env file
+dotenv.config();
 
+// get the PORT from the environment variables, OR use 4040 as default
+const PORT = process.env.PORT || 4040;
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (request, response, next) => {
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  response.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.json({ message: "Service healthy" });
+});
+
+app.get("/weather/:city", (request, response) => {
+  // Express adds a "params" Object to requests that has an matches parameter created using the colon syntax
+  const city = request.params.city;
+
+  // check if the request.query.cloudy attribute exists
+  let cloudy = "clear";
+  let rainy = false;
+  let lowTemp = 32;
+  if ("cloudy" in request.query) {
+    cloudy = request.query.cloudy;
+  }
+  if ("rainy" in request.query && request.query.rainy === "true") {
+    rainy = request.query.rainy;
+  }
+  if ("lowtemp" in request.query) {
+    lowTemp = Number(request.query.lowtemp);
+  }
+  // Teranery
+  // const cloudy = "cloudy" in request.query ? request.query.cloudy : "clear";
+  // const rainy =
+  //   "rainy" in request.query && request.query.rainy === "true" ? true : false;
+  // const lowTemp =
+  //   "lowtemp" in request.query ? Number(request.query.lowtemp) : 32;
+
+  // Generate a random number to use as the temperature
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+  const min = 70;
+  const max = 90;
+  const temp = Math.floor(Math.random() * (max - min + 1) + min);
+  // handle GET request for weather with an route parameter of "city"
+  response.json({
+    text: `The weather in ${city} is ${temp} degrees today.`,
+    cloudy: cloudy,
+    // When the key and value variable are named the same you can omit the value variable
+    rainy,
+    temp: {
+      current: temp,
+      low: lowTemp
+    },
+    city
+  });
+});
+
+app.post("/add", (request, response) => {
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2
+  };
+  response.json(responseBody);
+});
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
